9e9758c429ec0711fca863408e0be421dc2f4aa0
Ubuntu <ubuntu@ip-172-31-43-115.us-east-2.compute.internal>
2019-10-03 18:35:52 +0000

    Seeding to populate DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+courselist = [{:name => 'Math', :prof => 'John Doe'},
+              {:name => 'English', :prof => 'Jane Smith'},
+              {:name => 'Chemistry', :prof => 'John Oxygen'},
+              {:name => 'History', :prof => 'Mary Oldstuff'},
+              {:name => 'Spanish', :prof => 'Dave Spain'},
+              {:name => 'Linear Algebra', :prof => 'Mark Matrix'},
+              {:name => 'Probability', :prof => 'Tom Bayes'}
+              ]
+
+courselist.each do |course|
+    Course.create!(course)
+end
