484ec615f4a1301f1893adb82b91dbb6b6189a02
Ubuntu <ubuntu@ip-172-31-28-4.us-east-2.compute.internal>
2019-10-17 16:05:52 +0000

    seeded data for milestone1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Course.delete_all
+Notesheet.delete_all
+
 courselist = [{:name => 'Math', :prof => 'John Doe'},
               {:name => 'English', :prof => 'Jane Smith'},
               {:name => 'Chemistry', :prof => 'John Oxygen'},
@@ -13,9 +16,32 @@ courselist = [{:name => 'Math', :prof => 'John Doe'},
               {:name => 'Spanish', :prof => 'Dave Spain'},
               {:name => 'Linear Algebra', :prof => 'Mark Matrix'},
               {:name => 'Probability', :prof => 'Tom Bayes'},
-              {:name => 'Anthropology', :prof => 'Lebron James'}
+              {:name => 'Anthropology', :prof => 'Lebron James'},
+              {:name => 'French', :prof => 'Kevin Durant'},
+              {:name => 'Music Theory', :prof => 'Kawhi Leonard'},
+              {:name => 'Finance', :prof => 'Steph Curry'},
+              {:name => 'Marketing', :prof => 'Alex Caruso'},
+              {:name => 'Management', :prof => 'Giannis Antetokoumpo'},
+              {:name => 'Literature', :prof => 'Zion Williamson'},
+              {:name => 'Physics', :prof => 'Kyrie Irving'},
+              {:name => 'Biology', :prof => 'James Harden'},
+              {:name => 'Organic Chemitry', :prof => 'Russell Westbrook'},
+              {:name => 'Calculus', :prof => 'Anthony Davis'},
+              {:name => 'Legal Studies', :prof => 'Joel Embiid'},
+              {:name => 'Statistics', :prof => 'Klay Thompson'},
+              
               ]
 
 courselist.each do |course|
     Course.create!(course)
+end
+
+i = 0
+
+Course.all.each do |course|
+  
+  course.notesheets.create!(:content => "Sample note content", :course_id => course.id, :author_id => i, :date_created => "10/17/19")
+  i += 1
+  course.notesheets.create!(:content => "Different sample note content", :course_id => course.id, :author_id => i, :date_created => "10/18/19")
+  i += 1
 end
