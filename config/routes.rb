def9a0f35e9539a26b410eab46da608507acaa18
Mark Lisi <mlisi@tulane.edu>
2019-10-24 01:12:08 +0000

    Added scaffold for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :notesheets
   resources :courses
   root 'notesheets#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_161701) do
+ActiveRecord::Schema.define(version: 2019_10_24_010641) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2019_09_26_161701) do
     t.integer "course_id"
     t.integer "author_id"
     t.string "date_created"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "usertype"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
