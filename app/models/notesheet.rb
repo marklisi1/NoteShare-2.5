fa6fae43c8977c63e3478fe081f68fc2948d98e9
Mark Lisi <mlisi@tulane.edu>
2019-11-07 21:00:05 +0000

    Notesheet presence validations

--- app/models/notesheet.rb
+++ app/models/notesheet.rb
@@ -1,3 +1,7 @@
 class Notesheet < ApplicationRecord
     belongs_to :course
+    validates :content, presence: true
+    validates :course_id, presence: true
+    validates :author_id, presence: true
+    validates :date_created, presence: true
 end
