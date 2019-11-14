ab2789e4ea2397a6cfa52767627524d27688deb8
Mark Lisi <mlisi@tulane.edu>
2019-11-14 17:18:49 +0000

    Can add new users, usertype still shows 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_many :courses
-    enum usertype: [:student, :faculty, :admin]
     
     validates :name, presence: true
     validates :email, presence: true
--- db/migrate/20191030194548_add_usertype_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddUsertypeToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :usertype, :integer
-  end
-end
