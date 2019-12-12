56d4e60d32a5fc1fb1659b623b01b03f1777fff6
Ubuntu <ubuntu@ip-172-31-28-4.us-east-2.compute.internal>
2019-12-12 04:54:08 +0000

    cookie error adjustment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/profile' => 'home#profile'
+  get 'courses/enroll' => 'courses#enroll'
   get 'auth/:provider/callback', to: 'sessions#create'
   delete 'sign_out', to: 'sessions#destroy', as: 'sign_out'
   resources :users
