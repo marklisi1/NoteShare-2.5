4f1ed67286a9755ee7fdaefbf885aa665dc65064
Ubuntu <ubuntu@ip-172-31-28-4.us-east-2.compute.internal>
2019-12-12 05:59:32 +0000

    user can only delete own notesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   get 'home/profile' => 'home#profile'
-  get 'courses/enroll'
-  post 'courses/enrollcourse' => 'courses#enrollcourse'
-  
   get 'auth/:provider/callback', to: 'sessions#create'
   delete 'sign_out', to: 'sessions#destroy', as: 'sign_out'
   resources :users
